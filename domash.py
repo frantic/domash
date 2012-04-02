31c33d1c5832dfaa6a89c2f5dc9e615d7d676983
Frantic <alex.frantic@gmail.com>
2012-04-02 11:24:40 +0300

    Core functionality

--- domash.py
+++ domash.py
@@ -1,1 +1,10 @@
-print 'Init'
+import base64
+import time
+
+time.sleep(1)
+print base64.b64decode('SGFwcHkgYmlydGhkYXksIERvbWFzaCEgWW91IGFyZSBsZWdlbmQuLi4=')
+time.sleep(1)
+for i in xrange(5):
+	print '.'
+	time.sleep(0.5)
+print base64.b64decode('Li4uZGFyeSE=')
