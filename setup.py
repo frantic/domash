95d8090230516564605a8adef1e3473eb4315a21
Frantic <alex.frantic@gmail.com>
2012-04-02 11:17:42 +0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'domash',
+    version = '0.2.3',
+    py_modules = ['domash'],
+    author = 'Frantic',
+    author_email = 'alex.frantic@gmail.com',
+    url = 'http://frantic.im/',
+    description = 'Legendary Domash'
+    )
